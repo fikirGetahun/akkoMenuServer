e098cbca5c28f628b243088a4bc68b83f2e6201c
fikirGetahun <fikirgetahun97@gmail.com>
2023-02-27 16:58:17 +0300

    ff

--- middleware/admin.js
+++
@@ -1,5 +0,0 @@
-module.exports=function (req,res,next){
-    console.log(req.user)
-    if(!req.user.isAdmin) return res.status(403).send('Access denied');
-    next();
-}
--- middleware/logger2.js
+++
@@ -1,9 +0,0 @@
-function log(req,res,next){
-    console.log('Lodding . . .')
-    next();
-}
-function Authenticate(req,res,next){
-    console.log('Authenticate me . . .')
-    next();
-}
-module.exports ={log,Authenticate};
